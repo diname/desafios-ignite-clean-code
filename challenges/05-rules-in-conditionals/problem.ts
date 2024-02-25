0cf5a9b7cf4fcb695f50804ab23d5418c41f0060
Lucas Medina <lucasmedinarmc@gmail.com>
2024-02-24 22:19:01 -0300

    feat: add problem of challenge 5

---
+++ challenges/05-rules-in-conditionals/problem.ts
@@ -0,0 +1,26 @@
+const necessaryGradeToBeApproved = 7
+const studentGrade = 10
+
+const numberOfAbsencesToFailSchool = 100
+const studentNumberOfAbsences = 109
+
+function checkIfStudentPassedTheSchoolYear() {
+  if (studentGrade < necessaryGradeToBeApproved) {
+    return {
+      error: true,
+      message:
+        'Student was not approved because his grade was below the necessary.'
+    }
+  } else {
+    if (studentNumberOfAbsences > numberOfAbsencesToFailSchool) {
+      return {
+        error: true,
+        message: 'Student was not approved because of his absences'
+      }
+    } else {
+      return { error: false, message: 'Student was approved :)' }
+    }
+  }
+}
+
+console.log(checkIfStudentPassedTheSchoolYear())
