e0e478df1612740fdf6154b3a2cad215248fe1d9
Lucas Medina <lucasmedinarmc@gmail.com>
2024-02-25 12:44:11 -0300

    feat: add problem of challenge 6

---
+++ challenges/06-parameters-and-destructuring/problem.tsx
@@ -0,0 +1,11 @@
+function UpdateUserRoute(body, params) {
+  UpdateUserController(body, params)
+}
+
+function UpdateUserController(data, params) {
+  UserRepository.update(data, params)
+}
+
+const UserRepository = {
+  update: (data, params) => {}
+}
