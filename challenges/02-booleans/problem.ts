8efc7b2bced816ce9919b10dc20b4ce7cb0ef9fd
Lucas Medina <lucasmedinarmc@gmail.com>
2024-02-24 21:14:49 -0300

    feat: add content to problem of challenge 02

---
+++ challenges/02-booleans/problem.ts
@@ -0,0 +1,31 @@
+// Booleans
+
+const user = {
+  name: 'Diego Fernandes',
+  height: 190,
+  hasTicket: true
+}
+
+const necessaryHeight = 130
+
+const currentHour = new Date().getHours()
+
+const parkOpen = currentHour >= 9 && currentHour < 18
+
+if (!parkOpen) {
+  throw new Error('The park is closed!')
+}
+
+const ticket = user.hasTicket
+
+if (!ticket) {
+  throw new Error(`Diego doesn't have a ticket to enter the park!`)
+}
+
+const enterToy = user.height > necessaryHeight
+
+if (!enterToy) {
+  throw new Error(`Diego can't enter the toy!`)
+}
+
+console.log('Diego had a lot of fun! :)')
