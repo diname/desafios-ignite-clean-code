93b7b1cb8ac8e4c7846d70d35d093907134e42df
Lucas Medina <lucasmedinarmc@gmail.com>
2024-02-24 21:09:40 -0300

    refactor: add file to problem 02 to problem and the solution of the challenge

---
+++ challenges/02-booleans/solution.ts
@@ -0,0 +1,29 @@
+const user = {
+  name: 'Diego Fernandes',
+  height: 190,
+  hasTicket: true
+}
+
+const necessaryHeight = 130
+
+const currentHour = new Date().getHours()
+
+const isParkOpen = currentHour > 9 && currentHour < 18
+
+if (!isParkOpen) {
+  throw new Error('O parque está fechado!')
+}
+
+const userHasTicket = user.hasTicket
+
+if (!userHasTicket) {
+  throw new Error('O Diego não possui um bilhete para entrar no parque!')
+}
+
+const userCanEnterTheToy = user.height > necessaryHeight
+
+if (!userCanEnterTheToy) {
+  throw new Error('O Diego não pode entrar no brinquedo!')
+}
+
+console.log('O Diego se divertiu muito! :)')
