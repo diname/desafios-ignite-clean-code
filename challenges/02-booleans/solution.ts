2510856f61ffaeeab1d2170a8b3f3ee2fbeba074
Lucas Medina <lucasmedinarmc@gmail.com>
2024-02-24 21:15:07 -0300

    feat: add solution to challenge 02

--- challenges/02-booleans/solution.ts
+++ challenges/02-booleans/solution.ts
@@ -1,29 +1,32 @@
-const user = {
-  name: 'Diego Fernandes',
-  height: 190,
-  hasTicket: true
+function parkTrip() {
+  const userInformation = {
+    name: 'Diego Fernandes',
+    height: 190,
+    hasTicket: true
+  }
+
+  const minimunRequiredForHeight = 130
+
+  const currentHour = new Date().getHours()
+
+  const isParkOpen = currentHour > 9 && currentHour < 18
+
+  if (!isParkOpen) {
+    throw new Error('O parque está fechado!')
+  }
+
+  const userHasTicket = userInformation.hasTicket
+
+  if (!userHasTicket) {
+    throw new Error('O Diego não possui um bilhete para entrar no parque!')
+  }
+
+  const userCanEnterTheToy = userInformation.height > minimunRequiredForHeight
+
+  if (!userCanEnterTheToy) {
+    throw new Error('O Diego não pode entrar no brinquedo!')
+  }
+
+  console.log('O Diego se divertiu muito! :)')
 }
-
-const necessaryHeight = 130
-
-const currentHour = new Date().getHours()
-
-const isParkOpen = currentHour > 9 && currentHour < 18
-
-if (!isParkOpen) {
-  throw new Error('O parque está fechado!')
-}
-
-const userHasTicket = user.hasTicket
-
-if (!userHasTicket) {
-  throw new Error('O Diego não possui um bilhete para entrar no parque!')
-}
-
-const userCanEnterTheToy = user.height > necessaryHeight
-
-if (!userCanEnterTheToy) {
-  throw new Error('O Diego não pode entrar no brinquedo!')
-}
-
-console.log('O Diego se divertiu muito! :)')
+parkTrip()
