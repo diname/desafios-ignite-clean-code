94d4237e3e0b0ba606f471b368359a0be7db4cb7
Lucas Medina <lucasmedinarmc@gmail.com>
2024-02-26 14:18:44 -0300

    feat: add problem of challenge 9

---
+++ challenges/09-syntatic-sugars/problem.ts
@@ -0,0 +1,26 @@
+function _getFirstFiveRatings(ratings) {
+  return ratings.length >= 5 && ratings.slice(0, 5)
+}
+
+function _sumFirstFiveRatings(ratings) {
+  const ratingsBool = !!ratings
+
+  if (ratingsBool) {
+    const firstFiveRatings = _getFirstFiveRatings(ratings)
+
+    if (!firstFiveRatings) return { error: 'there must be at least 5 ratings' }
+
+    let ratingsSum = 0
+
+    for (const rating of firstFiveRatings) {
+      ratingsSum += +rating
+    }
+
+    return { ratingsSum, created_at: +new Date() }
+  }
+
+  return { error: 'ratings is required' }
+}
+
+const _appRatings = ['5', '3', '4', '4', '5', '1', '5', '4', '4', '3']
+_sumFirstFiveRatings(_appRatings)
